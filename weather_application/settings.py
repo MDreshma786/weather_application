852dd11320cf0be6434a100f87ef7e3695f65d61
Md.reshma <mohammadreshma11786@gmail.com>
2026-07-31 12:38:49 +0530

    Update ALLOWED_HOSTS

--- weather_application/settings.py
+++ weather_application/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = ['weather-application-1-jz21.onrender.com','localhost']
-ALLOWED_HOSTS=[ ]
+ALLOWED_HOSTS = ['weather-application-1-jz21.onrender.com','localhost']
+# ALLOWED_HOSTS=[ ]
 
 
 # Application definition
